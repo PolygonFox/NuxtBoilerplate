fd500e4b2600cf34e8547529efada06492971756
PolygonFox <github@polygonfox.com>
2017-06-20 21:35:17 +0200

    Initial Commit

---
+++ config/nuxt.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const resolve = require('path').resolve
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Adonuxt',
+    meta: [
+      {
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Adonuxt project'
+      }
+    ],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: 'favicon.ico'
+      }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: ['~assets/css/main.css'],
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#744d82' },
+  /*
+  ** Point to resources
+  */
+  srcDir: resolve(__dirname, '..', 'resources'),
+
+  build: {
+    loaders: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000, // 10KO
+          name: 'img/[name].[hash].[ext]'
+        }
+      }
+    ]
+  }
+}
